1a5f77696dabddbba33df9b65d40ccf8b2e7a956
Carla Reis <carlareis.dev@gmail.com>
2022-09-15 22:03:48 -0400

    using api data and rendering it

--- server/services/render.js
+++ server/services/render.js
@@ -1,5 +1,16 @@
+const axios = require("axios");
+
 exports.homeRoutes = (req, res) => {
-  res.render("index");
+  // make a get request to api/dishes
+  axios
+    .get("http://localhost:3000/api/dishes")
+    .then(function(response) {
+      console.log(response.data);
+      res.render("index", { dishes: response.data });
+    })
+    .catch(err => {
+      res.send(err);
+    });
 };
 
 exports.add_dish = (req, res) => {
